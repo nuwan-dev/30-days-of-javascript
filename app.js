7b6af0c9478c3626cb993b94ee9a7126182041eb
nuwan-dev <nuwanc.email@gmail.com>
2023-12-18 20:43:15 +0530

    c

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ is a
 multiline
 comment */
 
-let name = "Nimal"; //string
+/* let name = "Nimal"; //string
 let age = 30; //number
 let married = true; //boolean
 let graduated; //undefined
-let hasChilden = null; //object type
+let hasChilden = null; //object type 
 
 console.log(name + " is " + age + " years old, is he married? " + married);
 
 name[0] = "B";
-console.log(name);
+console.log(name); */
+
+let numbers = [0, 1, 2];
+numbers[0] = 10;
+
+console.log(numbers[0]);
