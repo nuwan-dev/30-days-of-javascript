dd1025c2b991212e43f9fe4221b7d408b6bd184e
nuwan-dev <nuwanc.email@gmail.com>
2023-12-18 14:30:12 +0530

    c

---
+++ app.js
@@ -0,0 +1,14 @@
+// Day 01
+// comments can make code readable
+/* This
+is a
+multiline
+comment */
+
+let name = "Nimal"; //string
+let age = 30; //number
+let married = true; //boolean
+let graduated; //undefined
+let hasChilden = null; //object type
+
+console.log(typeof hasChilden);
