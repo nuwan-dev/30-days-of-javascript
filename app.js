046dc4abb28446fc5a1b6fb88715f86326ea4cf0
nuwan-dev <nuwanc.email@gmail.com>
2023-12-20 20:23:47 +0530

    strings

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let randomNum = Math.floor(Math.random() * 10) + 1;
-console.log(randomNum);
+// let randomNum = Math.floor(Math.random() * 10) + 1;
+// console.log(randomNum);
+
+// strings
+
+let firstName = "Nimal";
+let lastName = "Bass";
+let live = "Warawala";
+
+console.log(firstName + lastName + live);
