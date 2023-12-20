28bcc5101902979a2281a64917221da0d7562c33
nuwan-dev <nuwanc.email@gmail.com>
2023-12-20 21:51:13 +0530

    c

--- app.js
+++ app.js
@@ -3,8 +3,20 @@
 
 // strings
 
-let firstName = "Nimal";
+/*let firstName = "Nimal";
 let lastName = "Bass";
 let live = "Warawala";
 
-console.log(firstName + " " + lastName + " " + live);
+console.log(firstName + " " + lastName + " " + live); */
+console.log(
+  "I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?"
+); // line break
+console.log("Days\tTopics\tExercises");
+console.log("Day 1\t3\t5");
+console.log("Day 2\t3\t5");
+console.log("Day 3\t3\t5");
+console.log("Day 4\t3\t5");
+console.log("This is a backslash  symbol (\\)"); // To write a backslash
+console.log('In every programming language it starts with "Hello, World!"');
+console.log("In every programming language it starts with 'Hello, World!'");
+console.log("The saying 'Seeing is Believing' isn't correct in 2020");
